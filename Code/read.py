d3513da225515aa11630aeef5f25fac7fe9ed4ac
sidharthvnair02 <82643868+sidharthvnair02@users.noreply.github.com>
2022-06-03 18:28:32 +0530

    Update read.py

--- Code/read.py
+++ Code/read.py
@@ -12,4 +12,18 @@ Y=df.price
 X=df.drop(['price'],axis=1)
 # print(X)
 
+X_train,X_test,Y_train,Y_test= train_test_split(X,Y, test_size=0.20, random_state=25)
+# # print(f"No. of training examples: {training_data.shape[0]}")
+# # print(f"No. of testing examples: {testing_data.shape[0]}")
+print(X_train.shape,Y_train.shape)
+
+print(X_test.shape,Y_test.shape)
+
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error , r2_score
+
+#defining the regression model
+model=linear_model.LinearRegression()
+# df = df.replace(r'^\s*$', np.nan, regex=True)
+#building the training model
+model.fit(X_train,Y_train)
